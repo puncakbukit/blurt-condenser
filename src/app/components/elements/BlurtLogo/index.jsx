3e5194d2b8fa7b1afafae123c33220d21a522c77
prazjp <26822588+puncakbukit@users.noreply.github.com>
2025-08-30 20:21:35 +0900

    Fix image/logo.png

--- src/app/components/elements/BlurtLogo/index.jsx
+++ src/app/components/elements/BlurtLogo/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BlurtLogo = () => {
     return (
         <span className="logo">
-            <img alt="Logo" src={`${process.env.PUBLIC_URL}/images/logo.png`} width="150" height="40" />
+            <img alt="Logo" src="images/logo.png" width="150" height="40" />
         </span>
     );
 };
