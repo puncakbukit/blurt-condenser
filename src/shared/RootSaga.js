596858e1abc85c1814f88fdd5fed13189b9dcbf3
prazjp <26822588+puncakbukit@users.noreply.github.com>
2025-08-31 19:41:08 +0900

    Fix src/shared/RootSaga.js

--- src/shared/RootSaga.js
+++ src/shared/RootSaga.js
@@ -6,13 +6,26 @@ import { authWatches } from 'app/redux/AuthSaga';
 import { transactionWatches } from 'app/redux/TransactionSaga';
 import { watchPollingTasks } from 'app/redux/PollingSaga';
 
+// Utility to filter out any saga we don't want in SPA mode
+function filterWatches(watches) {
+    return watches.filter(w => {
+        // skip sagas that reference syncSpecialPosts
+        if (w.name && w.name.toLowerCase().includes('special')) {
+            console.warn(`Disabled saga watcher: ${w.name}`);
+            return false;
+        }
+        return true;
+    });
+}
+
 export default function* rootSaga() {
     yield all([
         ...userWatches, // keep first to remove keys early when a page change happens
         ...fetchDataWatches,
-        ...sharedWatches,
+        ...filterWatches(sharedWatches),
         ...authWatches,
         ...transactionWatches,
         watchPollingTasks(),
     ]);
 }
+
